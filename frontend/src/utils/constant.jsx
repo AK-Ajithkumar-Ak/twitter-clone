7b672439fa8aba93c7c7449eaa32930e4f451a71
AK-Ajithkumar-Ak <axieinfinity502@gmail.com>
2025-09-17 09:03:31 +0530

    URL changed

--- frontend/src/utils/constant.jsx
+++ frontend/src/utils/constant.jsx
@@ -1,1 +1,4 @@
-export const BaseUrl= "http://localhost:3000"
+// export const BaseUrl= "http://localhost:3000"
+
+export const BaseUrl= import.meta.env.NODE_ENV =="development"? "http://localhost:3000" :""
+// https://twitter-clone-6wcl.onrender.com  => production
