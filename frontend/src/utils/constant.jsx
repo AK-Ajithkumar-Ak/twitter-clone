80cf9c657d3e4e71ba3c7def15b1a0f58e9d7930
AK-Ajithkumar-Ak <axieinfinity502@gmail.com>
2025-09-17 10:07:04 +0530

    production url change

--- frontend/src/utils/constant.jsx
+++ frontend/src/utils/constant.jsx
@@ -1,4 +1,5 @@
 // export const BaseUrl= "http://localhost:3000"
 
-export const BaseUrl= import.meta.env.NODE_ENV =="development"? "http://localhost:3000" :""
+export const BaseUrl= "https://twitter-clone-6wcl.onrender.com"
+// export const BaseUrl= import.meta.env.NODE_ENV =="development"? "http://localhost:3000" :""
 // https://twitter-clone-6wcl.onrender.com  => production
